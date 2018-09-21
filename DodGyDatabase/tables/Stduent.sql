7ca8c2b0987d7cd35d007bc8ca07f6d07abd9ef9
Adrian Phillips <101644658@student.swin.edu.au>
2018-09-21 14:31:01 +1000

    Innitial Commit

---
+++ DodGyDatabase/tables/Stduent.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Student]
+(
+	[StudentId] NVARCHAR(50) NOT NULL,
+	[FirstName] NVARCHAR(50) NOT NULL,
+	[LastName] NVARCHAR(50) NOT NULL,
+	[MacAddress] NVARCHAR(50) NULL,
+	[IssueDate] DATETIME NULL,
+	
+	PRIMARY KEY ([StudentId]),
+	FOREIGN KEY (MacAddress, IssueDate) REFERENCES Module(MacAddress, IssueDate)
+
+)
---
+++ DodGyWebAPI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DodGyWebAPI.Startup))]
+namespace DodGyWebAPI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
